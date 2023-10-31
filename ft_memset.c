62479baeebd3c64bc637f6cfca5b24236c30267c
3IMAD69 <dhaimy.imad@gmail.com>
2023-10-31 18:57:24 +0100

    add return

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:59:09 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/10/31 16:59:12 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/10/31 18:37:27 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ void    *ft_memset(void *s, int c, size_t n)
         unsigned char *ss = (unsigned char *)s;
         while (i < n)
                 ss[i++] = c;
+        return (s);
 }
