a5758a07474283a4ef6179e90c5dc5fa666938d4
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-01 16:12:30 +0100

    memcpy fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 14:18:01 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/01 15:49:22 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/01 16:06:19 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    const unsigned char *str1 = (const unsigned char *)s1;
-    const unsigned char *str2= (const unsigned char *)s2;
+    const unsigned char *str1;
+    const unsigned char *str2;
     unsigned int i;
 
     i = 0;
+    str1 = (const unsigned char *)s1;
+    str2= (const unsigned char *)s2;
     if (n == 0)
         return (0);
     while(str1[i] != '\0' && str2[i] != '\0' && str1[i] == str2[i] && i < n -1)
