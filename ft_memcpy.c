383170db0207c81bf75c70487eb9e8b065e77923
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-01 10:32:03 +0100

    handle dest = null case

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:01:34 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/10/31 17:16:40 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/01 10:31:08 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
+    if (dest)
+        return (NULL);
     char *destination = (char *) dest;
     const char *source = (const char *)src;
     while(n-- > 0)
