a00492ea6d6708a0b9d0c3f312c7be35b707a8cf
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-01 18:12:22 +0100

    added libft

--- libft.h
+++ libft.h
@@ -11,9 +11,14 @@ int ft_isascii(int c);
 int ft_isprint(int c);
 size_t ft_strlen (const char *s);
 void    *ft_memset(void *s, int c, size_t n);
-void bzero(void *s, size_t n);
+void ft_bzero(void *s, size_t n);
 void    *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
 
+
+
+
+
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
