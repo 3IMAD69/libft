361fd415640116fed70c2ef703943ed72db4dafa
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-02 16:02:35 +0100

    fixed dest null case

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,15 +6,15 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:01:34 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/01 10:31:08 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/02 16:02:13 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
-    if (dest)
+    if (!dest)
         return (NULL);
     char *destination = (char *) dest;
     const char *source = (const char *)src;
