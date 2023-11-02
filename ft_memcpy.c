7d931872192bb126e601d2f549ca3fb4c9653f7b
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-02 16:07:39 +0100

    clean

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,12 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:01:34 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/02 16:02:13 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/02 16:07:17 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
 void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
     if (!dest)
