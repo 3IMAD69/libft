6e12a8b8d16c5e0310211a66c15a87a514af5a21
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-03 16:53:21 +0100

    libft update

--- libft.h
+++ libft.h
@@ -4,6 +4,7 @@
 typedef long unsigned int size_t;
 # include <stdlib.h>
 # include <stdint.h>
+# include <unistd.h>
 
 int ft_isalpha(int c);
 int ft_isdigit(int c);
@@ -17,7 +18,8 @@ void    *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
 char *ft_strdup(const char *s);
-
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
 
 
 
