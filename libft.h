7d9e84b08ce82295ca5029d928cb5962b72252f1
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-03 22:50:50 +0100

    update header

--- libft.h
+++ libft.h
@@ -32,6 +32,8 @@ void ft_putstr_fd(char *s, int fd);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 t_list *ft_lstnew(void *content);
 void ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
 
 
 
