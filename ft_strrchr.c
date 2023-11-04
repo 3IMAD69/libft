6c80c619250c6b0f4603fa3c4347776229c00e19
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-04 20:02:59 +0100

    handle overflow + '0'

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:08:22 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/01 12:13:47 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/04 20:02:12 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 char *ft_strrchr(const char *s, int c)
 {
     char *tmp;
+    unsigned char character;
 
     tmp = NULL;
+    character = (unsigned char) c;
     while (*s)
     {
-        if (*s == c)
+        if (*s == character)
             tmp = (char *)s;
         s++;
     }
+    if (*s == character)
+        return (char *)s;
     return (tmp);
 }
