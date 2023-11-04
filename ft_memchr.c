80d22eeb26908bec12a49d9adc781388980901d1
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-04 18:15:39 +0100

    fixed some edge cases

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:48:55 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/01 14:17:12 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/04 18:13:52 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void *ft_memchr(const void *s, int c, size_t n)
 {
     const unsigned char * ss = (const unsigned char  *)s;
-    while (*ss && n-- > 0)
+    while (n-- > 0)
     {
         if (*ss == (unsigned char)c)
             return (void *)ss;
