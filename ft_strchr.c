4df2c9e5a9a85a3eb6e5a979b895932963323e0d
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-04 19:56:29 +0100

    strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,17 +6,22 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 11:46:02 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/01 12:06:34 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/04 19:51:51 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_strchr(const char *s, int c)
 {
+    unsigned char character;
+
+    character = (unsigned char) c;
     while (*s)
     {
-        if (*s == c)
+        if (*s == character)
             return (char *)s;
         s++;
     }
+    if (*s == character)
+        return (char *)s;
     return (0);
 }
