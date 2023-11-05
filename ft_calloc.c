4eae276bfc8decd98a2e17528d2fb8a18ffeb0ed
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-05 15:29:44 +0100

    fixed (0,0) case

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 19:21:07 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/04 16:12:46 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/05 15:28:36 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void *ft_calloc(size_t nmemb, size_t size)
 {
     void *ptr;
     
-    if (nmemb == 0 || size == 0 || nmemb > SIZE_MAX / size)
+    if (nmemb > SIZE_MAX / size)
         return (0);
         
     ptr = malloc(nmemb * size);
