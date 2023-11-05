7764f9ad8896afc3f25bbd38db4b0a15fb346a03
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-05 18:38:31 +0100

    fixed split

--- ft_split.c
+++ ft_split.c
@@ -6,24 +6,24 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:11:26 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/03 12:01:28 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/05 18:05:57 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char **wrtie_to_arr(char const *s,char **arr,char *tmp_str)
+char **wrtie_to_arr(char const *s,char **arr,char *tmp_str,char c)
 {
     int i;
     int j;
     
     i = 0;
     j = 0;
-    if (s[0] != '\0')
+    if (s[0] != c)
         arr[j++] = ft_strdup(&tmp_str[0]);
     while (s[i] != '\0')
     {
-        if ((tmp_str[i] == '\0') && (tmp_str[i + 1] != '\0'))
+        if ((s[i] == c) && (s[i + 1] != c) && (s[i + 1] != '\0'))
             arr[j++] = ft_strdup(&tmp_str[i + 1]);
         i++;
     }
@@ -43,7 +43,7 @@ char **ft_split(char const *s, char c)
     tmp_str = ft_strdup(s);
     while (s[i] != '\0')
     {
-        if ((tmp_str[i] != c) && ((tmp_str[i + 1] == c) || (tmp_str[i + 1] == '\0')))
+        if ((s[i] != c) && ((s[i + 1] == c) || (s[i + 1] == '\0')))
         {
             size++;
             tmp_str[i + 1] = '\0';
@@ -51,16 +51,21 @@ char **ft_split(char const *s, char c)
         i++;
     }
     arr = (char **)malloc(sizeof(char *) * (size + 1));
-    return(wrtie_to_arr(s,arr,tmp_str));
+    if (size == 0)
+        arr[0] = '\0';
+    else
+        arr = wrtie_to_arr(s,arr,tmp_str,c);
+    return (arr);
 }
 
-//#include <stdio.h>
+// #include <stdio.h>
 
 // int main()
 // {
 //     char **strings;
 //     int i  = 0;
-//     strings = ft_split("Shadow Wizard Money Gang",' ');
+//     strings = ft_split("  tripouille  42  ",' ');
+//     //printf("%d\n",strings[0]);
 //      while (strings[i] != NULL) {
 //         printf("%s\n", strings[i]);
 //         i++;
