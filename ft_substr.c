f93a671417a1092bd9d510276bafe332c1fa8ee1
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-05 17:17:12 +0100

    substr edge cases

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:40:20 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/02 16:36:19 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/05 17:15:54 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    size_t size;
-    char *str;
-    
-    size = ft_strlen(s);
-    if (start > size)
-    {
-        str = (char *)malloc(1 * sizeof(char));
-        str[0] = '\0';
-        return (str);
-    }
-    str = (char *)malloc((len + 1) * sizeof(char));
-    if(!str)
-        return (0);
-    ft_strlcpy(str,(s+start),len + 1);
+size_t size;
+char *str;
+
+size = ft_strlen(s);
+if (start > size)
+{
+    str = (char *)malloc(1 * sizeof(char));
+    str[0] = '\0';
     return (str);
+}
+if((size - start) < len)
+    len = (size-start);
+str = (char *)malloc((len + 1) * sizeof(char));
+if(!str)
+    return (0);
+ft_strlcpy(str,(s+start),len + 1);
+return (str);
 }
