6610bc205c8c827746d4c7273a7c259b7b34deea
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-07 12:26:00 +0100

    fixed strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,31 +6,28 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 15:55:14 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/05 13:55:26 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/07 12:25:45 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+
 char *ft_strnstr(const char *big,	const char *little, size_t len)
 {
     size_t	i;
-	size_t	j;
+	size_t	j; 
 
 	i = 0;
 	j = 0;
 	if (*little == '\0')
-	{
 		return ((char *)big);
-	}
 	while (little[j] != '\0')
-	{
 		j++;
-	}
-	while (i < len && big[i] != '\0')
+	while (i < len && big[i] != '\0' && (i + j) <= len)
 	{
 		if (big[i] == little[0]
-			&& ft_strncmp(&big[i], &little[0], j ) == 0)
+			&& ft_strncmp(&big[i], little, j ) == 0)
 		{
 			return ((char *)&big[i]);
 		}
