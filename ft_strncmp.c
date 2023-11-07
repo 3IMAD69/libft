646e4a1c5a907205d26bcb095a7776e132e7319f
3IMAD69 <dhaimy.imad@gmail.com>
2023-11-07 13:13:48 +0100

    fixed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:18:15 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/01 12:45:09 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/07 13:13:33 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    unsigned int i;
+    size_t i;
 
     i = 0;
     if (n == 0)
         return (0);
     while(s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n -1)
         i++;
-    return (s1[i] - s2[i]);
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
