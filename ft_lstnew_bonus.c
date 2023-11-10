9f7fa94864128cb849632754d64158722e7df4e3
Imad Dhaimy <idhaimy@e1r5p2.1337.ma>
2023-11-10 18:12:24 +0100

    lstnew

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 17:44:49 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/04 14:47:08 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/10 17:52:02 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,12 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*newnode;
 
-	if (!content)
-		return (NULL);
 	newnode = (t_list *)malloc(sizeof(t_list));
 	if (newnode)
 	{
 		newnode->content = content;
 		newnode->next = NULL;
 	}
-	else
-		return (NULL);
 	return (newnode);
 }
 
