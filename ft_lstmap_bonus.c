97e9688aec7b6b04e2638dcc16a15bc6c8173ee4
Imad Dhaimy <idhaimy@e1r5p2.1337.ma>
2023-11-10 21:40:01 +0100

    norminette

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 12:31:42 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/10 18:20:28 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/10 19:58:47 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*current;
 	t_list	*newlist;
 	t_list	*newnode;
-	void *tmp_cont;
+	void	*tmp_cont;
 
 	if (!lst || !f || !del)
 		return (NULL);
