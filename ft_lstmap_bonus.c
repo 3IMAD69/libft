52c15ae53db2cd5575893c1a6ff6b4eeed5a590c
Imad Dhaimy <idhaimy@e1r5p2.1337.ma>
2023-11-12 10:18:47 +0100

    lstmap comments

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 12:31:42 by idhaimy           #+#    #+#             */
-/*   Updated: 2023/11/11 16:48:58 by idhaimy          ###   ########.fr       */
+/*   Updated: 2023/11/12 10:16:44 by idhaimy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,63 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (newlist);
 }
+
+// #include <stdio.h>
+
+// void	del(void *content)
+// {
+// 	free (content);
+// }
+
+// void	*f(void	*content)
+// {
+// 	return ((void *) ft_strlen(content));
+// }
+
+// int main()
+// {
+// 	t_list	*node1;
+// 	t_list	*node2;
+// 	t_list	*node3;
+// 	t_list	*node4;
+
+//     node1 = (t_list *) malloc (sizeof(t_list));
+//     node2 = (t_list *) malloc (sizeof(t_list));
+//     node3 = (t_list *) malloc (sizeof(t_list));
+//     node4 = (t_list *) malloc (sizeof(t_list));
+
+//     node1->content = NULL; // Initialize content pointers
+//     node2->content = NULL;
+//     node3->content = NULL;
+//     node4->content = NULL;
+
+//     node1->next = node2;
+//     node2->next = node3;
+//     node3->next = node4;
+//     node4->next = NULL;
+
+//     node1->content = ft_strdup("ana node 1");
+//     node2->content = ft_strdup("ana ghankun node 2");
+//     node3->content = ft_strdup("idan ana node 3");
+//     node4->content = ft_strdup("kys");
+
+//     t_list *r = ft_lstmap(node1, f, del);
+
+//     printf("%d\n", (int) r->content);
+//     t_list *r1 = r->next;
+//     printf("%d\n", (int) r1->content);
+//     t_list *r2 = r1->next;
+//     printf("%d\n", (int) r2->content);
+//     t_list *r3 = r2->next;
+//     printf("%d\n", (int) r3->content);
+
+//     free(node1);
+//     free(node2);
+//     free(node3);
+//     free(node4);
+
+//     free(r);
+//     free(r1);
+//     free(r2);
+//     free(r3);
+// }
